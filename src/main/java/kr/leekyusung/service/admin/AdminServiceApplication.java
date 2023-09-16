9f4178596c1d845b4cf1815be9b7511bbbf9f723
LeeKyuSung <ks5050577@gmail.com>
2023-09-16 20:46:50 +0900

    admin-service 세팅

--- src/main/java/kr/leekyusung/service/admin/AdminServiceApplication.java
+++ src/main/java/kr/leekyusung/service/admin/AdminServiceApplication.java
@@ -1,9 +1,13 @@
 package kr.leekyusung.service.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableAdminServer
+@EnableDiscoveryClient
 public class AdminServiceApplication {
 
     public static void main(String[] args) {
