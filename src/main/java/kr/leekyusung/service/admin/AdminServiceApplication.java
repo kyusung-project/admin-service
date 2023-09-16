1808c20d26f7101d8c1c28df264861e09bdcdd2e
LeeKyuSung <ks5050577@gmail.com>
2023-09-16 21:14:50 +0900

    aws 최종 세팅
    discovery-service production url 추가
    
    Create EurekaConfiguration.java

--- src/main/java/kr/leekyusung/service/admin/AdminServiceApplication.java
+++ src/main/java/kr/leekyusung/service/admin/AdminServiceApplication.java
@@ -4,6 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -14,4 +21,19 @@ public class AdminServiceApplication {
         SpringApplication.run(AdminServiceApplication.class, args);
     }
 
+    @Bean
+    @Profile("prod")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        config.setNonSecurePort(80);
+        return config;
+    }
 }
